2a5f6f43d742e0ace2190f6a7b65f94c8f385db4
TeodorPeev <t.peev1@gmail.com>
2018-07-09 15:01:08 +0300

    PrintName problem added

---
+++ Solution1/ConsoleApp1/PrintNameMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PrintName
+{
+    class PrintNameMethod
+    {
+        static void Main()
+        {
+            PrintName("Teo");
+            PrintName(null);
+        }
+        static void PrintName(string name)
+        {
+            if(name == null)
+            {
+                Console.WriteLine("Hello guest");
+            }
+            else
+            {
+                Console.WriteLine($"Hello {name}");
+            }
+        }
+    }
+}
