beda23c390e836b3b91b12cfe124a7d01f1337ff
TeodorPeev <t.peev1@gmail.com>
2018-07-09 20:07:07 +0300

    Additional examples added

---
+++ Solution1/appearance-count-dictionary-solution/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace appearance_count_dictionary_solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short n = short.Parse(Console.ReadLine());
+            string numbers = Console.ReadLine();
+            string numberNeeded = Console.ReadLine();
+            Console.WriteLine(Regex.Matches(numbers, @"(^|\s)" + numberNeeded + @"($|\s)").Count);
+        }
+    }
+}
